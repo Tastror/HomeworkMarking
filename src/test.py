7da55cfa2c546487ed24fc42b311ddbc7fb3d56d
quyansiyuanwang <132176401+quyansiyuanwang@users.noreply.github.com>
2023-10-24 22:30:03 +0800

    for test

---
+++ src/test.py
@@ -0,0 +1,29 @@
+from handle_excel import *
+from mainWindow import ScoringWindow
+
+def test_excel():
+    excel_cur = ExcelIO('/testopt/tst.xlsx')
+    excel_cur.excelInit({
+        '学号': [123, 456],
+        '分数': [10, 80],
+        '建议': ['no', 'hhh']
+    })
+    
+    excel_cur.append([789, 90, 'yeah'])
+    excel_cur.dump()
+    
+def test_wind():
+    def callback():
+        return {
+            'text_left': '左边',
+            'text_right': '右边',
+            'text_bottom': '下面',
+            'text_top': '上面'
+        }
+    
+    wd = ScoringWindow(callback)
+    wd.run()
+    
+if __name__ == '__main__':
+    test_excel()
+    test_wind()

7f8b4cc13e2d4f42967b7127ee7b7311b670a3cd
quyansiyuanwang <132176401+quyansiyuanwang@users.noreply.github.com>
2023-10-24 21:37:26 +0800

    Create handle_excel.py

---
+++ src/handle_excel.py
@@ -0,0 +1,37 @@
+import pandas
+
+
+# {
+#    姓名 : [],
+#    作业 : []
+#    分数 : []
+#}
+class ExcelIO:
+    def __init__(self, file: str, expect_len: int):
+        self._file = file
+        self._expect_len = expect_len
+
+    def excelInit(self, 
+        field: dict[str: str | list[int, str]]
+        ) -> None:
+            for v in field.values():
+                v = [pandas.NaN] * self._expect_len
+            self.df = pandas.DataFrame(field)
+
+    def single_write(self, pos: tuple[int, str], info) -> None:
+        self.df.at(pos[0], pos[1]) = info
+
+    def group_write(self, idx, infos, axis: int = 0) -> bool:
+        match axis:
+            case 0 or 1:
+                self.df[idx] = info
+            case _:
+                return False
+        return True
+
+    def append(self, group: list) -> None:
+        self.df.append(group)
+
+    def dump(self, path: str, encoding: str = 'utf-8') -> None:
+        self.df.to_excel(path, encoding=encoding)    
+    
